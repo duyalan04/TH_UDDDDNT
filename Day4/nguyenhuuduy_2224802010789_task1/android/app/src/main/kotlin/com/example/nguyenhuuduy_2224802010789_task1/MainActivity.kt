e50dffbad2dbad4803f4898e623785cfcdac8403
ZuyAlan04 <nd83455@gamil.com>
2026-05-10 20:37:42 +0700

    update lab 5

---
+++ Day4/nguyenhuuduy_2224802010789_task1/android/app/src/main/kotlin/com/example/nguyenhuuduy_2224802010789_task1/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.nguyenhuuduy_2224802010789_task1
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ Day5/nguyenhuuduy_2224802010789_lab5/android/app/src/main/kotlin/com/example/nguyenhuuduy_2224802010789_lab5/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.nguyenhuuduy_2224802010789_lab5
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
